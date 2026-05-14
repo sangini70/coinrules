0c8c92919299214eb5f717e7b31898d421488b2a
your-name <your-email>
2026-05-15 00:14:23 +0900

    restore approutes only

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,5 @@
 import AppRoutes from './AppRoutes';
 
 export default function App() {
-  return (
-    <div style={{ padding: 0, margin: 0 }}>
-      <div>ROUTE_ROOT_ISOLATE</div>
-    </div>
-  );
+  return <AppRoutes />;
 }
